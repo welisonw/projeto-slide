d9b3f87c9924246b5b4348fc3eb5a3da45a00bab
Welison Wessling <welison.smi@hotmail.com>
2023-06-06 09:47:39 -0300

    Adicionada paginação por slide

--- js/script.js
+++ js/script.js
@@ -4,4 +4,6 @@ import { Slide, SlideNav } from "./slide.js";
 const slide = new SlideNav('.slide-wrapper', '.slide');
 slide.init();
 
-slide.addArrow('.prev', '.next')
+slide.addArrow('.prev', '.next');
+
+slide.addControl();
