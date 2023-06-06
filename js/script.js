46c6bd399b41d5735ce85a8d85c73030868fc48c
Welison Wessling <welison.smi@hotmail.com>
2023-06-06 10:23:41 -0300

    Custom controls estilizados

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 
-import { Slide, SlideNav } from "./slide.js";
+import SlideNav from "./slide.js";
 
 const slide = new SlideNav('.slide-wrapper', '.slide');
 slide.init();
 
 slide.addArrow('.prev', '.next');
 
-slide.addControl();
+slide.addControl('.custom-controls');
--- js/slide.js
+++ js/slide.js
@@ -197,7 +197,7 @@ export class Slide {
 
 
 
-export class SlideNav extends Slide {
+export default class SlideNav extends Slide {
    constructor(slide, wrapper) {
       super(slide, wrapper);
 
